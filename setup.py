dd53fa97556014678bae2f8b377a2a513a43c113
Sendo kame <sendo@sendokame.com>
2022-08-08 01:09:03 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ long_description = open('./README.md')
 setup(
     name='CliScrape',
     version='1.0.2',
-    url='https://github.com/ZSendokame/CliScrapper',
+    url='https://github.com/ZSendokame/CliScraper',
     license='MIT license',
     author='ZSendokame',
-    description='Webscrapping from the Terminal.',
+    description='Webscraping from the Terminal.',
     long_description=long_description.read(),
     long_description_content_type='text/markdown',
     py_modules=[
