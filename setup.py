e4bc2cd5ce299114cbffd83fb7594ea7dcfeee2e
Sendo kame <sendo@sendokame.com>
2022-08-13 04:25:59 -0400

    Metadata.

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 long_description = open('./README.md')
 
 setup(
     name='CliScrape',
-    version='1.0.2',
+    version='1.0.4',
     url='https://github.com/ZSendokame/CliScraper',
     license='MIT license',
     author='ZSendokame',
     description='Webscraping from the Terminal.',
     long_description=long_description.read(),
     long_description_content_type='text/markdown',
-    py_modules=[
-        'source.cs',
-    ],
+
+    packages=find_packages(include=['cliscraper.funct']),
 
     entry_points={
         'console_scripts': [
-            'cscrape=source.cs:main'
+            'cscrape=cliscraper.cs:main'
         ]
     },
 
     classifiers=[
-        'Environment :: Web Environment',
+        'Environment :: Web Environment'
     ]
 )
