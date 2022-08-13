e5c6529db0658b554356ac9743da83d42fdc7237
Sendo kame <sendo@sendokame.com>
2022-08-13 19:00:35 -0400

    Source code

--- cliscraper/cs.py
+++ cliscraper/cs.py
@@ -2,15 +2,11 @@ import arguing
 import requests
 from bs4 import BeautifulSoup
 
-import cliscraper.funct as funct
-
 
 def main():
     url = arguing.set('--url', mandatory=True, help_message='URL to scrape.')
     tag = arguing.set('--tag', help_message='Tags to filter.')
     selector = arguing.set('--selector', help_message='Selector to use.')
-    attributes = arguing.set('--attributes',
-                             help_message='HTML Node attributes.')
     output = arguing.set('--output', help_message='Output type (BS4).',
                          default='attrs')
 
@@ -21,11 +17,10 @@ def main():
 
     # Main
     tags = tag.split(',')
-    attribute_dict = funct.process_attributes(attributes)
     soup = BeautifulSoup(response.text, 'lxml')
 
     if arguing.check('--tag'):
-        nodes = soup.find_all(tags, attrs=attribute_dict)
+        nodes = soup.find_all(tags)
 
     elif arguing.check('--selector'):
         nodes = soup.select(selector)
